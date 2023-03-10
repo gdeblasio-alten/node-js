3273ebf14ce74e6f27feb4fff9860e595d0969e7
gdeblasio-alten <72490656+gdeblasio-alten@users.noreply.github.com>
2023-03-10 12:25:40 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// console.log(`Hello Node.js v${process.versions.node}!`);
+
+// Crea un'applicazione Node.js che prenda un array di numeri e ne calcoli la somma
+
+// Utilizzando Express.js, crea un server che risponda a una richiesta GET con un messaggio di saluto personalizzato in base al nome passato come parametro nell'URL.
